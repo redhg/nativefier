813c8548504f98dcea8a1556b78801730afb6608
Breon Halling <github@redhg.com>
2025-03-29 19:00:43 -0400

    Remove icon inferring logic

--- src/options/fields/icon.test.ts
+++ src/options/fields/icon.test.ts
@@ -1,12 +1,5 @@
-import * as log from 'loglevel';
-
 import { icon } from './icon';
-import { inferIcon } from '../../infer/inferIcon';
 
-jest.mock('./../../infer/inferIcon');
-jest.mock('loglevel');
-
-const mockedResult = 'icon path';
 const ICON_PARAMS_PROVIDED = {
   packager: {
     icon: './icon.png',
@@ -23,38 +16,12 @@ const ICON_PARAMS_NEEDS_INFER = {
 
 describe('when the icon parameter is passed', () => {
   test('it should return the icon parameter', async () => {
-    expect(inferIcon).toHaveBeenCalledTimes(0);
     await expect(icon(ICON_PARAMS_PROVIDED)).resolves.toBeUndefined();
   });
 });
 
 describe('when the icon parameter is not passed', () => {
-  test('it should call inferIcon', async () => {
-    (inferIcon as jest.Mock).mockImplementationOnce(() =>
-      Promise.resolve(mockedResult),
-    );
-    const result = await icon(ICON_PARAMS_NEEDS_INFER);
-
-    expect(result).toBe(mockedResult);
-    expect(inferIcon).toHaveBeenCalledWith(
-      ICON_PARAMS_NEEDS_INFER.packager.targetUrl,
-      ICON_PARAMS_NEEDS_INFER.packager.platform,
-    );
-  });
-
-  describe('when inferIcon resolves with an error', () => {
-    test('it should handle the error', async () => {
-      (inferIcon as jest.Mock).mockImplementationOnce(() =>
-        Promise.reject(new Error('some error')),
-      );
-      const result = await icon(ICON_PARAMS_NEEDS_INFER);
-
-      expect(result).toBeUndefined();
-      expect(inferIcon).toHaveBeenCalledWith(
-        ICON_PARAMS_NEEDS_INFER.packager.targetUrl,
-        ICON_PARAMS_NEEDS_INFER.packager.platform,
-      );
-      expect(log.warn).toHaveBeenCalledTimes(1); // eslint-disable-line @typescript-eslint/unbound-method
-    });
+  test('it should return the icon parameter', async () => {
+    await expect(icon(ICON_PARAMS_NEEDS_INFER)).resolves.toBeUndefined();
   });
 });
